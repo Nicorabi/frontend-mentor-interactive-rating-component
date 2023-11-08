b8c89aefbbd7277a9b82241be163b577a55ecf95
Nicorabi <121673697+Nicorabi@users.noreply.github.com>
2023-11-09 01:08:38 +1100

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ const secondContainerEl = document.querySelector("#second-container");
 const btnEl = document.querySelector("#btn");
 const ratingsEl = document.querySelector("#rating");
 const numbersEl = document.querySelectorAll(".numbers");
+let submit = null;
 
 btnEl.addEventListener("click", () => {
-  mainContainerEl.style.display = "none";
-  secondContainerEl.classList.remove("hidden");
-})
+  if (submit === null) {
+    alert("Please choose a number!");
+  } else { 
+    mainContainerEl.style.display = "none";
+    secondContainerEl.classList.remove("hidden");
+  }
+});
 
 numbersEl.forEach((number) => {
   number.addEventListener("click", () => {
     ratingsEl.textContent = number.textContent;
+    submit = true;
   })
 })
